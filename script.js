8f76e0172e5b678259c0f2775b83479e7b283594
Marc Mas <marc5481@iessineu.net>
2026-01-14 17:37:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function nombrePrimer() {
+    var n = parseInt(document.getElementById("n1").value);
+    if (n <= 1) {
+        return false;
+    }
+    if (n == 2) {
+        return true;
+    }
+    for (let i = 2, s = Math.sqrt(n); i <= s; i++) {
+        if (n % i === 0) return false;
+    }
+    return true;
+}
+
+function comprovar() {
+    if (nombrePrimer()) {
+        console.log("El nombre es primer")
+    } else {
+        console.log("El nombre no es primer")
+    }
+}
